50c14f8513a6473e23fa14b00855ec366d4640b8
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-18 11:24:40 +0200

    convert list from dummy_data.py in json / implement json from direct from python and use it single_gadget_view(request): for the HttpResponse / change content-type from text to Json

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .dummy_data import gadgets
+import json
 
 # Create your views here.
 
@@ -8,4 +9,4 @@ def start_page_view(request):
     return HttpResponse("hey das funktioniert ja :)!")
 
 def single_gadget_view(request):
-    return HttpResponse(gadgets[0])
+    return HttpResponse(json.dumps(gadgets[0]), content_type="application/json")
