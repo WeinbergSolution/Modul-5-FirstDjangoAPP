181e0cc95f934c47a4d5309bf630daca32e64fc4
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-18 10:26:37 +0200

    Implement dummy_data.py in tech_gadgets / create new view function single_gadget_view in views.py / extend import and path in urls.py

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .dummy_data import gadgets
 
 # Create your views here.
 
 def start_page_view(request):
     return HttpResponse("hey das funktioniert ja :)!")
+
+def single_gadget_view(request):
+    return HttpResponse(gadgets[0])
