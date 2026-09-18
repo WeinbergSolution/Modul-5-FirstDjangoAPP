10b0466501c454db677da0f4a5bc193ed0d1837b
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-18 22:13:07 +0200

    Add: create 404 Tamplate and registrate in settings.py

--- backend/your_project/settings.py
+++ backend/your_project/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'your_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'your_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
