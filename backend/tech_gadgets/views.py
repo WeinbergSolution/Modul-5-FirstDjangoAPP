f454ccd0e71788fdca8d8a9b2d26aa8e69ba406b
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-18 19:26:36 +0200

    refactor: convert gadget view to class-based view

--- backend/tech_gadgets/views.py
+++ backend/tech_gadgets/views.py
@@ -5,6 +5,8 @@ from django.utils.text import slugify
 from django.urls import reverse
 import json
 
+from django.views import View
+
 
 # Create your views here.
 
@@ -20,6 +22,39 @@ def single_gadget_int_view(request, gadget_id):
         return redirect(new_url)
 
     return HttpResponseNotFound("not found in list")
+
+
+class GadgetView(View):
+     def get(self, request, gadget_slug):
+         gadget_match = None
+      
+         for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+             gadget_match = gadget
+      
+        # WICHTIG: außerhalb der for-Schleife
+         if gadget_match:
+            return JsonResponse(gadget_match)
+      
+        # WICHTIG: ebenfalls außerhalb der for-Schleife
+         raise Http404()   
+
+
+     def post(self, request, *args, **kwargs):
+         try:
+              data = json.loads(request.body)
+         
+              print(f"received data: {data['test']}")
+         
+              return JsonResponse({"response": "perfekt :)"})
+         
+         except:
+            return JsonResponse({"response": "Das war wohl nix"})
+
+
+
+
+         
 
 
 def single_gadget_view(request, gadget_slug=""):
