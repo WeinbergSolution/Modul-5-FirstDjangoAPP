127b8e7436815716f82c46670fe9300812e5934f
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-18 23:38:13 +0200

    add: exerciese

---
+++ example/4. Sektion_4_Aufgabe.py
@@ -0,0 +1,24 @@
+### --------------- Backend Modul 5 Einstieg in Django ---------------- ###
+
+## --------------- Sektion 4: Aufgabe --------------------##
+
+# ------------   01 - Aufgabenstellung -------------- #
+
+#   In der dummy_data.py befinden sich neben den Gadgets auch
+#   Manufacturers, die wir bisher noch nicht verwendet oder angezeigt haben.
+#
+#   Aufgabe:
+#
+#   -> In der urls.py einen oder mehrere neue Pfade für die Manufacturers anlegen.
+#
+#   -> Eine GET-View erstellen, über die die Manufacturers abgerufen
+#      und angezeigt werden können.
+#
+#   -> Eine POST-View erstellen, über die ein neuer Manufacturer
+#      hinzugefügt werden kann.
+#
+#   -> In der test_w_post.html die vorhandene GET-URL und POST-URL
+#      entsprechend auf die neuen Manufacturer-Endpunkte anpassen.
+#
+#   GET und POST haben wir bereits zu Beginn des Projekts verwendet.
+#   Dieses Wissen soll nun auf die Manufacturers übertragen werden.
