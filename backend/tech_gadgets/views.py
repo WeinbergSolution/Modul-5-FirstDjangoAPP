b804169c5c4e5399b2914645676ebb7df2dffe23
WeinbergSolution <pascalgiercke@gmail.com>
2026-09-18 19:02:42 +0200

    refactor: combine GET and POST handling in gadget view

--- backend/tech_gadgets/views.py
+++ backend/tech_gadgets/views.py
@@ -1,39 +1,51 @@
 from django.shortcuts import redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, Http404
 from .dummy_data import gadgets
-import json
 from django.utils.text import slugify
 from django.urls import reverse
+import json
+
 
 # Create your views here.
 
 def start_page_view(request):
     return HttpResponse("hey das funktioniert ja :)!")
 
-def single_gadget_view(request, gadget_id):
+
+def single_gadget_int_view(request, gadget_id):
+
     if len(gadgets) > gadget_id:
         new_slug = slugify(gadgets[gadget_id]["name"])
         new_url = reverse("gadget_slug_url", args=[new_slug])
-
         return redirect(new_url)
+
     return HttpResponseNotFound("not found in list")
-    
-def single_gadget_slug_view(request,gadget_slug):
-    gadget_match = None
 
-    for gadget in gadgets:
-        if slugify(gadget["name"]) == gadget_slug:
-            gadget_match = gadget
 
-    if gadget_match:
-        return JsonResponse(gadget_match)
-    raise Http404()
+def single_gadget_view(request, gadget_slug=""):
 
-def single_gadget_post_view(request):
+    if request.method == "GET":
+        gadget_match = None
+
+        for gadget in gadgets:
+            if slugify(gadget["name"]) == gadget_slug:
+                gadget_match = gadget
+
+        # WICHTIG: außerhalb der for-Schleife
+        if gadget_match:
+            return JsonResponse(gadget_match)
+
+        # WICHTIG: ebenfalls außerhalb der for-Schleife
+        raise Http404()
+
+
     if request.method == "POST":
         try:
             data = json.loads(request.body)
-            print(f"recieved data: {data["test"]}")
+
+            print(f"received data: {data['test']}")
+
             return JsonResponse({"response": "perfekt :)"})
+
         except:
             return JsonResponse({"response": "Das war wohl nix"})
